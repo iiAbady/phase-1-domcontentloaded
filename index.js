f705f852020cd466f59661b38c31e5aebd902cfc
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-20 01:08:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+	const paragraph = document.querySelector('p#text')
+	paragraph.textContent = "This is really cool!";
+})
